a1c81e718aaed545cd311470818759d63655d4ec
Murali <muralisurya2004@gmail.com>
2026-01-28 00:33:50 +0530

    Create engine1.java

---
+++ autowiring(BY TYPE) /engine1.java
@@ -0,0 +1,18 @@
+
+package com.spring.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class engine1 
+{
+	private int engineid;
+    private String capacity;
+}
