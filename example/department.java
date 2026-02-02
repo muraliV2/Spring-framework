76da934088bc67a2524798224b205df3aba095ad
Murali <muralisurya2004@gmail.com>
2026-02-02 23:19:40 +0530

    Create department.java

---
+++ example/department.java
@@ -0,0 +1,12 @@
+package com.jspiders.javaBasedConfig.beans;
+
+import lombok.Data;
+
+@Data
+public class Department {
+
+    private int deptNo;
+    private String location;
+    private String deptName;
+
+}
