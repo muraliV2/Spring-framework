1eabb50e86887cabb0a760a9a3b980d823809e38
Murali <muralisurya2004@gmail.com>
2026-02-02 23:20:57 +0530

    Create mainclass.java

---
+++ example/mainclass.java
@@ -0,0 +1,16 @@
+package com.jspiders;
+
+import com.jspiders.javaBasedConfig.beans.Employee;
+import com.jspiders.javaBasedConfig.configuration.JavaConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfiguration.class);
+        System.out.println(context.getBean("employee1", Employee.class));
+        System.out.println(context.getBean("employee2", Employee.class));
+
+    }
+}
