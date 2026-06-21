f77c11ad64aa35150722b5235ee6be4f392b67e6
Murali <muralisurya2004@gmail.com>
2026-06-21 20:21:45 +0530

    Update employee.java

--- example/employee.java
+++ example/employee.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 @Data
 
 public class Employee {
