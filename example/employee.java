55278777469db19b127c70869c17149ad55e2ad0
Murali <muralisurya2004@gmail.com>
2026-06-21 20:22:40 +0530

    Update employee.java

--- example/employee.java
+++ example/employee.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 @Data
-
 public class Employee {
     @Value("${employeeId}")
     private int employeeId;
