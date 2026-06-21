ac048b0c1c9dc0502d8d1d530b1c58f48c25b15d
Murali <muralisurya2004@gmail.com>
2026-06-21 20:20:02 +0530

    Update mainclass.java

--- example/mainclass.java
+++ example/mainclass.java
@@ -5,7 +5,6 @@ import com.jspiders.javaBasedConfig.configuration.JavaConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfiguration.class);
