6c80b8ffca8a878a25c926022f1794a5d1809895
Murali <muralisurya2004@gmail.com>
2026-07-06 20:59:39 +0530

    Update configuration.java

--- example/configuration.java
+++ example/configuration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.*;
 @ComponentScan (basePackages = "com.jspiders.javaBasedConfig")
 @PropertySource("classpath:META-INF/config.properties")
 
-
 public class JavaConfiguration {
     @Bean
     public Department department() {
